93b139b50221b7c4313d44469029c64f4f403798
brianhouse <brian.house@gmail.com>
2016-06-08 22:23:01 -0600

    volume curves now working

--- TODO.md
+++ TODO.md
@@ -3,3 +3,6 @@ could find peaks of each bloom
 what's the pulse?
 - days
 - year / seasons
+
+
+I think the seasons need to be drawn out
--- player.py
+++ player.py
@@ -18,19 +18,19 @@ def p(n):
         return n
     return f
 
-signals = signals[0:5]
+signals = signals[0:8]
 
 # one cycle is half a month
 
-year = Voice(1, rate=1/24, chord=(C1, DOM))
+year = Voice(1, rate=1/24, chord=(C1, DOM), controls={'volume': 127})
 year.set([1, 0])
 # year.mute(True)
 
-month = Voice(2, rate=0.5, chord=(C2, DOM))
+month = Voice(2, rate=0.5, chord=(C2, DOM), controls={'volume': 127})
 month.set([1, 0])
 # month.mute(True)
 
-days = Voice(10, rate=1, chord=None)    # more like half a week
+days = Voice(10, rate=1, chord=None, controls={'volume': 127})
 days.set([b(54), p(54), b(54), p(54)] * 2)
 # days.mute(True)
 
@@ -49,23 +49,19 @@ days.set([b(54), p(54), b(54), p(54)] * 2)
 #             return n
 #     return f
 
-channels = [2, 3, 4, 5, 6, 7]     # why does the first one not play?
-notes = [1, 2, 3, 4, 5, 6]
+channels = [3, 4, 5, 6, 7, 8]
+notes =    [1, 2, 3, 4, 5, 6]
 for s, signal in enumerate(signals):
     if s == len(channels):
         break
+    print("channel", channels[s])
     f = signal_from_timeseries(signal)
     plot(f, color="red")
-    print('channel', channels[s])
-    v = Voice(channels[s], rate=1, chord=(C, LYD), controls={'volume': 7})
-    # v.set(patterns[s % len(patterns)])
-    v.set([s])
-    # v.velocity.set(0.0)
-    # v.velocity.tween(1.0, 24, f, repeat=True, flip=False)
+    v = Voice(channels[s], rate=1, chord=(C, LYD), controls={'volume': 20})
+    v.set([notes[s]])
     v.volume(10)
     v.volume.tween(127, 24, f, repeat=True, flip=False)
     v.add("playing")
-
     
 show_plots()
 play()
