b68ad4c2c4a4f4de99cd286152f35c58836fd239
brianhouse <brian.house@gmail.com>
2016-07-23 15:17:55 -0400

    sounding good\!

--- phenology.py
+++ phenology.py
@@ -4,33 +4,16 @@ import csv, datetime
 import signal_processing as sp
 import numpy as np
 from random import random
-from housepy import util, drawing, strings
+from housepy import util, drawing, strings, config
 from collections import OrderedDict
 from colors import colors
 
-# START = "1973-01-01"
-START = "2014-01-01"
-# START = "2010-01-01"
-# END = "1973-12-31"
-END = "2015-12-31"
-
-NORMALIZE = True
-TAIL = 172800 # 2 days
-
-species_list = []
-# species_list = ['Taraxacum officinale', 'Collomia linearis', 'Hydrophyllum fendleri']
-# species_list = ['Festuca thurberi', 'Elymus glaucus', 'Salix sp.', 'Stellaria longifolia', 'Erigeron coulteri', 'Hydrophyllum capitatum', 'Ranunculus inamoenus', 'Epilobium brachycarpum', 'Erigeron elatior', 'Oxypolis fendleri']
-# species_list = ['Collomia linearis']
-# species_list = ['Carex nigricans']
-species_list = ["Claytonia lanceolata", "Mertensia fusiformis", "Hydrophyllum capitatum", "Erythronium grandiflorum", "Delphinium nuttallianum", "Adenolinum lewisii", "Erigeron flagellaris", "Delphinium barbeyi", "Dugaldia hoopesii", "Erigeron speciosus", "Heterotheca villosa"]
-
-
-def generate(onset_spikes=False, peak_spikes=False):
+def generate():
 
     # load data into t and count arrays per species
     species = OrderedDict()
-    start_t = util.timestamp(util.parse_date(START))
-    end_t = util.timestamp(util.parse_date(END))
+    start_t = util.timestamp(util.parse_date(str(config['start'])))
+    end_t = util.timestamp(util.parse_date(str(config['end'])))
     max_count = 0
     with open("data.csv") as f:
         data = csv.reader(f)
@@ -39,7 +22,7 @@ def generate(onset_spikes=False, peak_spikes=False):
                 continue
             plot = row[1]        
             name = row[2]        
-            if len(species_list) and name not in species_list:
+            if len(config['species_list']) and name not in config['species_list']:
                 continue
             dt = datetime.datetime(int(row[3]), 1, 1) + datetime.timedelta(int(row[4]) - 1)
             t = util.timestamp(dt)
@@ -68,9 +51,9 @@ def generate(onset_spikes=False, peak_spikes=False):
                 end_season_t = ts[i-1]
                 if i < len(ts):
                     start_season_t = ts[i]
-                    ts.insert(i, start_season_t - TAIL)
+                    ts.insert(i, start_season_t - config['tail'])
                     species[name]['counts'].insert(i, 0)
-                ts.insert(i, end_season_t + TAIL)
+                ts.insert(i, end_season_t + config['tail'])
                 species[name]['counts'].insert(i, 0)
         species[name]['ts'].append(end_t)
         species[name]['counts'].append(0)
@@ -86,7 +69,7 @@ def generate(onset_spikes=False, peak_spikes=False):
 
         # create signal from bloom counts
         signal = sp.resample(data['ts'], data['counts'])
-        if NORMALIZE:
+        if config['normalize']:
             signal = sp.normalize(signal)
         else:
             signal = sp.normalize(signal, 0, max_count)    
@@ -94,7 +77,7 @@ def generate(onset_spikes=False, peak_spikes=False):
         signal = sp.limit(signal, max(signal))  # get rid of noise below 0 for onset detection
 
         # add spikes for peaks
-        if peak_spikes:
+        if config['peak_spikes']:
             peaks, valleys = sp.detect_peaks(signal, lookahead=50)
             peak_signal = np.zeros(len(signal))    
             for peak in peaks:
@@ -102,15 +85,18 @@ def generate(onset_spikes=False, peak_spikes=False):
             signal += peak_signal
 
         # add spikes for onsets
-        if onset_spikes:
+        if config['onset_spikes']:
             onsets = sp.detect_onsets(signal)
             onset_signal = np.zeros(len(signal))    
             for onset in onsets:
                 onset_signal[onset] = 0.5
+                onset_signal[onset+1] = 0.4
+                onset_signal[onset+2] = 0.25
             signal += onset_signal
 
         # limit
         signal = sp.limit(signal, 1.0)
+        signal *= 0.9   # hack, just controlling gain
         signals.append(signal)   
 
         names.append(name)
@@ -131,3 +117,5 @@ if __name__ == "__main__":
         ctx.line(10 / ctx.width, 1 - ((10 + (i * 10)) / ctx.height), 30 / ctx.width, 1 - ((10 + (i * 10)) / ctx.height), stroke=color, thickness=2)
         ctx.label(35 / ctx.width, 1 - ((13 + (i * 10)) / ctx.height), names[i].upper(), size=10)    
     ctx.output("charts")
+
+    util.save("signals.pkl", signals)
